fab9220aea3eca922a18a0a118e88b35c38f79b8
amansawhney <amansawhney@sawhneygames.com>
2018-09-21 08:24:04 -0700

    added some documentation

--- variables.cpp
+++ variables.cpp
@@ -20,5 +20,17 @@ int main()
     A = 10;
 
     cout << A << endl;
+    //adress does not change
     cout << &A << endl;
+
+    int iPrimeNr; //Hungarian Notation
+
+    /*
+     *  1. Variables can't have the same name in the same scope
+     *  2. Variables can't start from numbers
+     *  3. Can't use spaces but can use _
+     *  4. Variables should be self-descriptive
+     *  5. Can't use special characters and all that jazz
+     *  6. Variables should be nouns
+     */
 }
