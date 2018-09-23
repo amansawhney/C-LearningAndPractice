a1bb1eb6f2dd6734697187e80e818bd5bc92cf89
amansawhney <amansawhney@sawhneygames.com>
2018-09-23 16:27:15 -0700

    finished lecture 1

--- variables.cpp
+++ variables.cpp
@@ -11,19 +11,42 @@ using namespace std;
 
 int main()
 {
-    int A = 4; //4 bytes of memory -> 32 bits for 0s and 1s
-    // get address => &name_of_variable;
+    int a = 40, b, c=20; //-2bill to 2bill, 4 BYTES
+    //the value for the address of b isn't removed until it is initialized
+    b = 0;
+    cout << "a : " << a << " a address : " << &a << endl;
+    cout << "b : " << b << " b address : " << &b << endl;
+    cout << "c : " << c << " c address : " << &c << endl;
 
-    cout << A << endl;
-    cout << &A << endl;
+    short int t1 = 5; //-32768 to 32767 2 BYTES
+    cout << "t1 : " << t1 << " t1 address : " << &t1 << endl;
 
-    A = 10;
+    float t2 = 5.12; //4 BYTES that are up to 38 zeros
+    double t3 = 5.12; //8 BYTES that are up to 308 zeros + greater precision
 
-    cout << A << endl;
-    //adress does not change
-    cout << &A << endl;
+    char t4; //character
 
-    int iPrimeNr; //Hungarian Notation
+    t4 = 'a';
+    cout << t4 << endl;
+
+    string t5 = "Hello ";
+    string t6 = "World";
+
+    cout << t5 + t6<< endl;
+
+    bool t7 = true; //false is always 0 and all other are true
+
+    cout << t7 << endl;
+
+    //unsigned short int, 0 to 65535
+
+    unsigned short t8 = 3700;
+    cout << t8 << endl;
+
+    const string NAMEOFGAME = "Conqueror of C++"; //consts are uppercase
+
+    cout << NAMEOFGAME << endl;
+
 
     /*
      *  1. Variables can't have the same name in the same scope
